417ccdb2ba22bcfdaf7413533833a5d9382ee0ac
Drifter8 <73453936+Drifter8@users.noreply.github.com>
2020-12-22 15:25:07 -0800

    Add files via upload

---
+++ zone.js
@@ -0,0 +1,18 @@
+class Zone {
+    constructor(x,y,width,height){
+        var option = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,option)
+        this.height = height
+        this.width = width
+        World.add(world,this.body)
+    }
+
+    display(){
+        var pos = this.body.position
+        rectMode(CENTER)
+        fill("red")
+        rect(pos.x,pos.y,this.width,this.height)
+    }
+}
